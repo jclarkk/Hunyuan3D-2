2c4397250a5c8e12d6b470747c74ef8c56e6bba7
Jonathan Clark <jonathan@hexa3d.io>
2025-01-22 11:23:56 +0200

    Support text prompt for texture CLI

--- texture.py
+++ texture.py
@@ -6,20 +6,28 @@ from PIL import Image
 
 from hy3dgen.rmbg import preprocess_image
 from hy3dgen.texgen import Hunyuan3DPaintPipeline
+from hy3dgen.text2image import HunyuanDiTPipeline
 
 
 def run(args):
-    # Only one image supported right now
-    image_path = args.image_paths[0]
+    if args.prompt is None and args.image_paths is None:
+        raise ValueError("Please provide either a prompt or an image")
+
+    if args.prompt is not None and args.image_paths is not None:
+        raise ValueError("Please provide either a prompt or an image, not both")
+
+    if args.prompt is not None:
+        t2i = HunyuanDiTPipeline('Tencent-Hunyuan/HunyuanDiT-v1.1-Diffusers-Distilled')
+        image = t2i(args.prompt)
+    else:
+        # Only one image supported right now
+        image_path = args.image_paths[0]
+        image = Image.open(image_path)
 
     t0 = time.time()
 
     # Preprocess the image
-    image = Image.open(image_path)
     image = preprocess_image(image)
-
-    processed_image_name = os.path.basename(image_path).split('.')[0] + '_input.png'
-    image.save(os.path.join(args.output_dir, processed_image_name))
 
     t1 = time.time()
     print(f"Image processing took {t1 - t0:.2f} seconds")
@@ -30,7 +38,7 @@ def run(args):
     # Generate texture
     t4 = time.time()
     pipeline = Hunyuan3DPaintPipeline.from_pretrained('tencent/Hunyuan3D-2')
-    mesh = pipeline(mesh, image=image_path, texture_size=args.texture_size)
+    mesh = pipeline(mesh, image=image, texture_size=args.texture_size)
     t5 = time.time()
     print(f"Texture generation took {t5 - t4:.2f} seconds")
 
@@ -48,8 +56,9 @@ def run(args):
 if __name__ == "__main__":
     # Parse arguments and then call run
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image_paths', type=str, nargs='+', required=True,
+    parser.add_argument('--image_paths', type=str, nargs='+', default=None,
                         help='Path to input images. Can specify multiple paths separated by spaces')
+    parser.add_argument('--prompt', type=str, default=None, help='Prompt for the image')
     parser.add_argument('--mesh_path', type=str, help='Path to input mesh', required=True)
     parser.add_argument('--output_dir', type=str, default='./output', help='Path to output directory')
     parser.add_argument('--seed', type=int, default=0, help='Seed for the random number generator')
