8591da2ed06aff7d8e75c169243a9d9aa8a666f4
jclarkk <jclarkk@gmail.com>
2025-06-02 17:09:44 +0300

    Fix import path for pb_solver

--- pb_solver/compile_pb_solver.py
+++ pb_solver/compile_pb_solver.py
@@ -1,4 +1,4 @@
-from mvadapter.utils.mesh_utils.blend import PBTorchCUDAKernelBackend
+from hy3dgen.texgen.mvadapter.utils.mesh_utils.blend import PBTorchCUDAKernelBackend
 
 # This will trigger compilation and cache the .so in TORCH_EXTENSIONS_DIR
 print("Precompiling pb_solver...")
