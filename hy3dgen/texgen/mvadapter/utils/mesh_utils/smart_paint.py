901ba01f58ce746e1a80a9a3d74673ed7a17102f
jclarkk <jclarkk@gmail.com>
2025-06-03 10:56:50 +0300

    Changed params

--- hy3dgen/texgen/mvadapter/utils/mesh_utils/smart_paint.py
+++ hy3dgen/texgen/mvadapter/utils/mesh_utils/smart_paint.py
@@ -280,7 +280,7 @@ class SmartPainter:
                     from_scratch=False,
                     poisson_blending=False,
                     depth_grad_dilation=3,
-                    uv_exp_blend_alpha=3,
+                    uv_exp_blend_alpha=12,
                     aoi_cos_valid_threshold=0.1,
                     uv_size=mesh.uv_size,
                     uv_padding=True,
